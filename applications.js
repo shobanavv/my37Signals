0feabb0a9820a583da53366390dbefaee3f1724c
shobanavv <shobanavv@gmail.com>
2013-11-08 22:01:52 -0600

    newchanges almost final version

--- applications.js
+++ applications.js
@@ -1,42 +1,37 @@
+
 $(document).ready(function() {  
 
-$('#bc').mouseenter(function(){
-    $('h1').html("Base Camp is the project mangement tool you wish  your last project");
-$('.arrow').show();});
+    //Default h1 text
+    var defaultHeader = "Making collaboration productive and enjoyable for people every day.";
+    var defaultdescription="Frustration-free web-based apps for collaboration, sharing information, and making decisions."
+    //var bc=$('#bc').attr("href","http://basecamp.com/?source=37signals+home");
 
-$('#bc').mouseleave(function(){ $('h1').html("Making collaboration productive and enjoyable for people every day.");$('.arrow').hide();});
-
-$('#hr').mouseenter(function(){
-    $('h1').html("Highrise remembers the important thing about people normally forget.");
-$('.arrow').show();});
-
-$('#hr').mouseleave(function(){ $('h1').html("Making collaboration productive and enjoyable for people every day.");
-$('.arrow').hide();});
-
-$('#cf').mouseenter(function(){
-    $('h1').html("From near or far, campfire teams work together in real-time.");$('.arrow').show();});
-
-$('#cf').mouseleave(function(){ $('h1').html("Making collaboration productive and enjoyable for people every day.");$('.arrow').hide();});
-
-
-
-
-var key=$('#id').mouseenter();
-
-switch(key){
-    case 'bc':
-    
-        $('h1').html("Base Camp is the project mangement tool you wish you had on your last project");
-    
-    break;
-    case 'hr':
-
-        $('h1').html("Highrise remembers the important thing about people you'd  normally forget.");
-    break;
-    case 'cf':
-    $('h1').html("From near or far, campfire teams work together in real-time.");
-    default:
-    
-        $('h1').html("Making collaboration productive and enjoyable for people every day.");
-    }
+    //Change h1 to Basecampp header
+    $('#bc').mouseenter(function(){
+        $('h1').html("<span text-decoration:underline; color:#336699;>BaseCamp</span>is the project mangement tool you wish  your last project");
+        $('#main_descrip').html("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.");
+        $('#bc_arrow').show();
     });
+
+    //Change h1 to Highrise header
+    $('#hr').mouseenter(function(){
+        $('h1').html("Highrise remembers the important thing about people normally forget.");
+        $('#main_descrip').html("Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.");
+        $('#hr_arrow').show();
+    });
+
+    //Change h2 to campfire text
+    $('#cf').mouseenter(function(){
+        $('h1').html("From near or far, campfire teams work together in real-time.");
+        $('#main_descrip').html("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It's game changing. We couldn't run our own business without Campfire.");
+        $('#cf_arrow').show();
+    });
+
+    //Change h1 back to default header
+    $('.logo_img').mouseleave(function(){ 
+        $('h1').html(defaultHeader);
+        $('#main_descrip').html(defaultdescription);
+        $('.arrow').hide();
+    });
+
+});
